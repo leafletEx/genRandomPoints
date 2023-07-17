f51cb1f8b0d655038a6a4bdf7890f048c980ca2c
zcj <18137693952@163.com>
2023-07-17 16:38:40 +0800

    加载高德地图

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import L from "leaflet";
+import gcoord from "gcoord";
+import { reactive, onMounted } from "vue";
+
+let mapObj = reactive({});
+const initMap = () => {
+  L.TileLayer.GaoDeTileLayer = L.TileLayer.extend({
+    initialize: function (param, options) {
+      const templateUrl =
+        "//wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&{p}";
+      // var templateUrl = "//webst{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&{p}"
+      options = L.extend(
+        {
+          p: param,
+          subdomains: "1234",
+          minZoom: 0,
+          maxZoom: 23,
+          minNativeZoom: 1,
+          maxNativeZoom: 18,
+        },
+        options,
+      );
+      L.TileLayer.prototype.initialize.call(this, templateUrl, options);
+    },
+    _setZoomTransform: function (level, center, zoom) {
+      center = L.latLng(
+        gcoord
+          .transform([center.lng, center.lat], gcoord.WGS84, gcoord.GCJ02)
+          .reverse(),
+      ); // 采用 gcoord 库进行纠偏
+      L.TileLayer.prototype._setZoomTransform.call(this, level, center, zoom);
+    },
+    _getTiledPixelBounds: function (center) {
+      center = L.latLng(
+        gcoord
+          .transform([center.lng, center.lat], gcoord.WGS84, gcoord.GCJ02)
+          .reverse(),
+      ); // 采用 gcoord 库进行纠偏
+      return L.TileLayer.prototype._getTiledPixelBounds.call(this, center);
+    },
+  });
+
+  L.tileLayer.gaoDeTileLayer = function (param, options) {
+    return new L.TileLayer.GaoDeTileLayer(param, options);
+  };
+
+  const c6000_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=6&ltype=0&scl=0&size=0",
+    ), // 影像底图
+    c8000_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=8&ltype=0&scl=0&size=0",
+    ), // 影像标注，路网 + 注记
+    c8200_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=8&ltype=2&scl=0&size=0",
+    ), // 影像标注，路网
+    c8400_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=8&ltype=4&scl=0&size=0",
+    ), // 影像标注，注记
+    c7000_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=0&scl=0&size=0",
+    ), // 电子地图，区域面 + 路网 + 注记 + 楼块
+    c7100_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=1&scl=0&size=0",
+    ), // 电子底图，区域面
+    c7200_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=2&scl=0&size=0",
+    ), // 电子标注，路网
+    c7300_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=3&scl=0&size=0",
+    ), // 电子底图，区域面 + 路网
+    c7400_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=4&scl=0&size=0",
+    ), // 电子标注，注记
+    c7500_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=5&scl=0&size=0",
+    ), // 电子底图，区域面 + 注记
+    c7600_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=6&scl=0&size=0",
+    ), // 电子标注，路网 + 注记
+    c7700_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=7&scl=0&size=0",
+    ), // 电子底图，区域面 + 路网 + 注记
+    c7800_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=8&scl=0&size=0",
+    ), // 电子底图，楼块
+    c7020_Layer = L.tileLayer.gaoDeTileLayer(
+      "lang=zh_cn&style=7&ltype=0&scl=2&size=0",
+    ); // 电子底图，区域面 + 路网 + 楼块
+
+  const map = L.map("map", {
+    center: [29.70805, 118.321499],
+    zoom: 15,
+    zoomControl: false,
+    attributionControl: false,
+    doubleClickZoom: false,
+  });
+
+  map.addLayer(c7000_Layer);
+
+  Object.assign(mapObj, map);
+};
+
+onMounted(() => {
+  initMap();
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="map" id="map"></div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.map {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
+import 'ress'
+import "leaflet/dist/leaflet.css"
 
 createApp(App).mount('#app')
