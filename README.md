254a6786e555f50a9185e78d2666493c2a476a93
zcj <18137693952@163.com>
2023-07-18 14:47:39 +0800

    feat: 底图切换

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# Vue 3 + Vite
+## 底图参数
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+```js
+  const layerOpts = {
+    c6000_Layer: "lang=zh_cn&style=6&ltype=0&scl=0&size=0", // 影像底图
+    c8000_Layer: "lang=zh_cn&style=8&ltype=0&scl=0&size=0", // 影像标注，路网 + 注记
+    c8200_Layer: "lang=zh_cn&style=8&ltype=2&scl=0&size=0", // 影像标注，路网
+    c8400_Layer: "lang=zh_cn&style=8&ltype=4&scl=0&size=0", // 影像标注，注记
+    c7000_Layer: "lang=zh_cn&style=7&ltype=0&scl=0&size=0", // 电子地图，区域面 + 路网 + 注记 + 楼块
+    c7100_Layer: "lang=zh_cn&style=7&ltype=1&scl=0&size=0", // 电子底图，区域面
+    c7200_Layer: "lang=zh_cn&style=7&ltype=2&scl=0&size=0", // 电子标注，路网
+    c7300_Layer: "lang=zh_cn&style=7&ltype=3&scl=0&size=0", // 电子底图，区域面 + 路网
+    c7400_Layer: "lang=zh_cn&style=7&ltype=4&scl=0&size=0", // 电子标注，注记
+    c7500_Layer: "lang=zh_cn&style=7&ltype=5&scl=0&size=0", // 电子底图，区域面 + 注记
+    c7600_Layer: "lang=zh_cn&style=7&ltype=6&scl=0&size=0", // 电子标注，路网 + 注记
+    c7700_Layer: "lang=zh_cn&style=7&ltype=7&scl=0&size=0", // 电子底图，区域面 + 路网 + 注记
+    c7800_Layer: "lang=zh_cn&style=7&ltype=8&scl=0&size=0", // 电子底图，楼块
+    c7020_Layer: "lang=zh_cn&style=7&ltype=0&scl=2&size=0", // 电子底图，区域面 + 路网 + 楼块
+};
+```
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import L from "leaflet";
 import gcoord from "gcoord";
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, ref } from "vue";
 
 // 初始化高德地图底图
 const initGaoDeTileLayer = () => {
@@ -42,27 +42,30 @@ initGaoDeTileLayer();
 
 // 根据类型获取高德底图
 const getGaoDeLayerByType = (type) => {
-  const layerOpts = {
-    c6000_Layer: "lang=zh_cn&style=6&ltype=0&scl=0&size=0", // 影像底图
-    c8000_Layer: "lang=zh_cn&style=8&ltype=0&scl=0&size=0", // 影像标注，路网 + 注记
-    c8200_Layer: "lang=zh_cn&style=8&ltype=2&scl=0&size=0", // 影像标注，路网
-    c8400_Layer: "lang=zh_cn&style=8&ltype=4&scl=0&size=0", // 影像标注，注记
-    c7000_Layer: "lang=zh_cn&style=7&ltype=0&scl=0&size=0", // 电子地图，区域面 + 路网 + 注记 + 楼块
-    c7100_Layer: "lang=zh_cn&style=7&ltype=1&scl=0&size=0", // 电子底图，区域面
-    c7200_Layer: "lang=zh_cn&style=7&ltype=2&scl=0&size=0", // 电子标注，路网
-    c7300_Layer: "lang=zh_cn&style=7&ltype=3&scl=0&size=0", // 电子底图，区域面 + 路网
-    c7400_Layer: "lang=zh_cn&style=7&ltype=4&scl=0&size=0", // 电子标注，注记
-    c7500_Layer: "lang=zh_cn&style=7&ltype=5&scl=0&size=0", // 电子底图，区域面 + 注记
-    c7600_Layer: "lang=zh_cn&style=7&ltype=6&scl=0&size=0", // 电子标注，路网 + 注记
-    c7700_Layer: "lang=zh_cn&style=7&ltype=7&scl=0&size=0", // 电子底图，区域面 + 路网 + 注记
-    c7800_Layer: "lang=zh_cn&style=7&ltype=8&scl=0&size=0", // 电子底图，楼块
-    c7020_Layer: "lang=zh_cn&style=7&ltype=0&scl=2&size=0", // 电子底图，区域面 + 路网 + 楼块
+  const layerObj = {
+    "01": { opts: "lang=zh_cn&style=6&ltype=0&scl=0&size=0", info: "影像底图" },
+    "02": {
+      opts: "lang=zh_cn&style=7&ltype=0&scl=0&size=0",
+      info: "电子地图底图",
+    },
   };
 
-  return new L.TileLayer.GaoDeTileLayer(layerOpts[type], {});
+  return new L.TileLayer.GaoDeTileLayer(layerObj[type].opts, {});
 };
 
-let mapObj = reactive({});
+let mapObj = ref();
+
+const curMapLayer = ref();
+const setMapLayer = (type) => {
+  if (curMapLayer.value) {
+    curMapLayer.value.remove(mapObj.value);
+    curMapLayer.value = null;
+  }
+
+  curMapLayer.value = getGaoDeLayerByType(type);
+  curMapLayer.value.addTo(mapObj.value);
+};
+
 const initMap = () => {
   // leaflet 默认投影是 L.CRS.EPSG3857	与高德相同，所以无需设置
   const map = L.map("map", {
@@ -73,13 +76,13 @@ const initMap = () => {
     doubleClickZoom: false,
   });
 
-  map.addLayer(getGaoDeLayerByType("c7000_Layer"));
+  mapObj.value = map;
+
+  setMapLayer("02");
 
   map.on("click", (e) => {
     console.log(e, "====");
   });
-
-  Object.assign(mapObj, map);
 };
 
 onMounted(() => {
@@ -88,12 +91,46 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="map" id="map"></div>
+  <div class="map-box">
+    <div class="map" id="map"></div>
+
+    <div class="operate">
+      <p @click="setMapLayer('01')">影像地图</p>
+      <p @click="setMapLayer('02')">电子地图</p>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.map {
+<style lang="scss" scoped>
+.map-box {
+  position: relative;
   width: 100vw;
   height: 100vh;
+
+  .map {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .operate {
+    position: absolute;
+    z-index: 9990;
+    top: 10px;
+    left: 10px;
+    display: flex;
+
+    p {
+      color: #ffffff;
+      border-radius: 4px;
+      padding: 4px 6px;
+      margin-left: 10px;
+      background: cadetblue;
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+  }
 }
 </style>
