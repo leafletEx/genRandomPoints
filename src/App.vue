8ca360149ee7fb489360b5b96d5c5b3c0354726a
zcj <18137693952@163.com>
2023-07-18 11:11:58 +0800

    加载地图初始化

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import L from "leaflet";
 import gcoord from "gcoord";
 import { reactive, onMounted } from "vue";
 
-let mapObj = reactive({});
-const initMap = () => {
+// 初始化高德地图底图
+const initGaoDeTileLayer = () => {
   L.TileLayer.GaoDeTileLayer = L.TileLayer.extend({
     initialize: function (param, options) {
       const templateUrl =
@@ -40,63 +40,43 @@ const initMap = () => {
       return L.TileLayer.prototype._getTiledPixelBounds.call(this, center);
     },
   });
+};
 
-  L.tileLayer.gaoDeTileLayer = function (param, options) {
-    return new L.TileLayer.GaoDeTileLayer(param, options);
+initGaoDeTileLayer();
+
+// 根据类型获取高德底图
+const getGaoDeLayerByType = (type) => {
+  const layerOpts = {
+    c6000_Layer: "lang=zh_cn&style=6&ltype=0&scl=0&size=0", // 影像底图
+    c8000_Layer: "lang=zh_cn&style=8&ltype=0&scl=0&size=0", // 影像标注，路网 + 注记
+    c8200_Layer: "lang=zh_cn&style=8&ltype=2&scl=0&size=0", // 影像标注，路网
+    c8400_Layer: "lang=zh_cn&style=8&ltype=4&scl=0&size=0", // 影像标注，注记
+    c7000_Layer: "lang=zh_cn&style=7&ltype=0&scl=0&size=0", // 电子地图，区域面 + 路网 + 注记 + 楼块
+    c7100_Layer: "lang=zh_cn&style=7&ltype=1&scl=0&size=0", // 电子底图，区域面
+    c7200_Layer: "lang=zh_cn&style=7&ltype=2&scl=0&size=0", // 电子标注，路网
+    c7300_Layer: "lang=zh_cn&style=7&ltype=3&scl=0&size=0", // 电子底图，区域面 + 路网
+    c7400_Layer: "lang=zh_cn&style=7&ltype=4&scl=0&size=0", // 电子标注，注记
+    c7500_Layer: "lang=zh_cn&style=7&ltype=5&scl=0&size=0", // 电子底图，区域面 + 注记
+    c7600_Layer: "lang=zh_cn&style=7&ltype=6&scl=0&size=0", // 电子标注，路网 + 注记
+    c7700_Layer: "lang=zh_cn&style=7&ltype=7&scl=0&size=0", // 电子底图，区域面 + 路网 + 注记
+    c7800_Layer: "lang=zh_cn&style=7&ltype=8&scl=0&size=0", // 电子底图，楼块
+    c7020_Layer: "lang=zh_cn&style=7&ltype=0&scl=2&size=0", // 电子底图，区域面 + 路网 + 楼块
   };
 
-  const c6000_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=6&ltype=0&scl=0&size=0",
-    ), // 影像底图
-    c8000_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=8&ltype=0&scl=0&size=0",
-    ), // 影像标注，路网 + 注记
-    c8200_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=8&ltype=2&scl=0&size=0",
-    ), // 影像标注，路网
-    c8400_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=8&ltype=4&scl=0&size=0",
-    ), // 影像标注，注记
-    c7000_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=0&scl=0&size=0",
-    ), // 电子地图，区域面 + 路网 + 注记 + 楼块
-    c7100_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=1&scl=0&size=0",
-    ), // 电子底图，区域面
-    c7200_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=2&scl=0&size=0",
-    ), // 电子标注，路网
-    c7300_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=3&scl=0&size=0",
-    ), // 电子底图，区域面 + 路网
-    c7400_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=4&scl=0&size=0",
-    ), // 电子标注，注记
-    c7500_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=5&scl=0&size=0",
-    ), // 电子底图，区域面 + 注记
-    c7600_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=6&scl=0&size=0",
-    ), // 电子标注，路网 + 注记
-    c7700_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=7&scl=0&size=0",
-    ), // 电子底图，区域面 + 路网 + 注记
-    c7800_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=8&scl=0&size=0",
-    ), // 电子底图，楼块
-    c7020_Layer = L.tileLayer.gaoDeTileLayer(
-      "lang=zh_cn&style=7&ltype=0&scl=2&size=0",
-    ); // 电子底图，区域面 + 路网 + 楼块
+  return new L.TileLayer.GaoDeTileLayer(layerOpts[type], {});
+};
 
+let mapObj = reactive({});
+const initMap = () => {
   const map = L.map("map", {
-    center: [29.70805, 118.321499],
+    center: [39.87, 116.38],
     zoom: 15,
     zoomControl: false,
     attributionControl: false,
     doubleClickZoom: false,
   });
 
-  map.addLayer(c7000_Layer);
+  map.addLayer(getGaoDeLayerByType("c7000_Layer"));
 
   Object.assign(mapObj, map);
 };
