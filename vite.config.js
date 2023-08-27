5de1ec9d5e3d18a9e255c7b82fa406f37ccc496a
zcj <18137693952@163.com>
2023-08-27 17:27:47 +0800

    feat: 部署路径

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+    console.log(mode, '----------\r\n')
     return {
-        base: '/leaflet-gaode/',
+        base: mode === 'development' ? '/' : '/leaflet-gaode/',
         plugins: [vue()],
     }
 })
