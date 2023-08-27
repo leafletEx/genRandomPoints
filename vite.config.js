7c1527fd5a56e83dcd7326edf7b47a328905536b
zcj <18137693952@163.com>
2023-08-27 20:30:09 +0800

    feat: 移除 gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-    console.log(mode, '----------\r\n')
-    return {
-        base: mode === 'development' ? '/' : '/leaflet-gaode/',
-        plugins: [vue()],
-    }
+export default defineConfig({
+    plugins: [vue()],
 })
