59382dfb52da1613deb66a3c33d8f405714170d3
zcj <18137693952@163.com>
2023-11-03 21:48:10 +0800

    feat: 增加一键复制功能

--- src/components/ViewGeneratePointsDrawer.vue
+++ src/components/ViewGeneratePointsDrawer.vue
@@ -1,37 +1,61 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from "vue";
+import ClipboardJS from "clipboard";
+import { ElMessage } from "element-plus";
 
+const initClipboard = () => {
+  const clipboard = new ClipboardJS(document.getElementById("clipboardBtn"));
 
-const viewData = ref('')
-const showDrawer = ref(false)
+  clipboard.on("success", (e) => {
+    ElMessage.success("复制成功！");
+    e.clearSelection();
+  });
+
+  clipboard.on("error", (e) => {
+    // 数据存在，复制失败进行提示！
+    if (e.text) ElMessage.warning("复制失败！");
+    else ElMessage.warning("需要复制的数据为空！");
+  });
+};
+
+const viewData = ref("");
+const showDrawer = ref(false);
 const openDrawer = (data) => {
-  viewData.value = data
-  showDrawer.value = true
-}
+  viewData.value = data;
+  showDrawer.value = true;
+  initClipboard();
+};
 
 defineExpose({
-  openDrawer
-})
+  openDrawer,
+});
 </script>
 
 <template>
-  <el-drawer v-model="showDrawer"
-             size="50%"
-             direction="ltr"
-             :show-close="false"
-             :with-header="false">
+  <el-drawer
+    v-model="showDrawer"
+    size="50%"
+    direction="ltr"
+    :show-close="false"
+    :with-header="false"
+  >
     <el-scrollbar style="height: 90vh">
       <div>
         {{ viewData }}
       </div>
     </el-scrollbar>
 
-    <footer>
-      <el-button type="primary">复制</el-button>
-    </footer>
+    <template #footer>
+      <el-row justify="end">
+        <el-button
+          type="success"
+          id="clipboardBtn"
+          :data-clipboard-text="viewData"
+          >一键复制数据
+        </el-button>
+      </el-row>
+    </template>
   </el-drawer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
